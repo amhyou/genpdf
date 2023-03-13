7b1a23646d00b0e289ef569a25cbccbf27548eb9
amhyou <younes.amhaouch@hotmail.com>
2023-03-13 03:39:24 +0100

    pdf conversion via selenium devtools headless mode activated successfully

--- genpdf/urls.py
+++ genpdf/urls.py
@@ -2,27 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render
-import subprocess
-'''
-from django.template.loader import get_template
-import asyncio
-from pyppeteer import launch
-async def generate():
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto('http://127.0.0.1:8000/view/')
-    await page.pdf({ path: 'proposals/output.pdf', format: 'A4' })
-    await browser.close()
-# asyncio.get_event_loop().run_until_complete(generate())
-'''
+from pyhtml2pdf import converter
+
 def generate_pdf(request):
-    # await generate()
-    subprocess.check_call(['node', 'genpdf/genpdf.js'])
-    with open('proposals/output.pdf', 'rb') as f:
-        pdf_data = f.read()
+    target = "proposals/output.pdf"
+    pdf_data = converter.convert('http://127.0.0.1:8000/view/', target, print_options={"scale": 0.95})
     response = HttpResponse(pdf_data, content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename=output.pdf'
-    #response['Content-Disposition'] = 'inline; filename=output.pdf'
+    response['Content-Disposition'] = 'inline; filename=output.pdf'
     return response
 
 def view_pdf(request):
